90a4f2bb504603e5f614e4fef4288d40280385b7
Dom Patmore <dom@pilifter.com>
2024-03-14 11:46:56 +0000

    frame score '00 00' => 0

--- tests/test_bowling_score.py
+++ tests/test_bowling_score.py
@@ -1,16 +1,22 @@
 def get_score_from_frame(frame):
-    if frame == "X":
-        return 10
-    if frame[1] == "/":
+    if frame == "X" or frame[1] == "/":
         return 10
     first_roll = int(frame[0])
     second_roll = int(frame[1])
 
     return first_roll + second_roll
 
-def test_strike_equals_10():
-    assert get_score_from_frame("X") == 10
+def get_score_from_game(game):
+    frames = game.split(" ")
+    total = 0
+    for frame in frames:
+        total += get_score_from_frame(frame)
+    return total
 
+
+
+def test_frame_with_strike():
+    assert get_score_from_frame("X") == 10
 
 def test_frame_no_score():
     assert get_score_from_frame("00") == 0
@@ -20,3 +26,7 @@ def test_frame_with_score():
 
 def test_frame_with_spare():
     assert get_score_from_frame("4/") == 10
+
+def test_two_frames_with_no_score():
+    assert get_score_from_game("00 00") == 0
+
