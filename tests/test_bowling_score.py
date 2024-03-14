276123175d4473639a872efaa6c75ec11e57fedd
Dom Patmore <dom@pilifter.com>
2024-03-14 11:09:54 +0000

    test simple frame with no score

--- tests/test_bowling_score.py
+++ tests/test_bowling_score.py
@@ -1,6 +1,13 @@
 def get_score_from_roll(roll):
-    return 10
+    if roll == "X":
+        return 10
+    else:
+        return 0
 
 # test a single frame with a strike
 def test_strike_equals_10():
     assert get_score_from_roll("X") == 10
+
+# test a single frame without strike
+def test_simple_frame_no_score():
+    assert get_score_from_roll("00") == 0
