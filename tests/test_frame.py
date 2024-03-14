3c458b2cd41e73723b38738251ac41a5b2107965
Dom Patmore <dom@pilifter.com>
2024-03-14 15:24:28 +0000

    test: GREEN not spare => false

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,11 +1,29 @@
 class Frame: 
+    def __init__(self, rolls):
+        self.rolls = rolls
+
     def is_strike(self):
-        return False
-frame = Frame()
+        return self.rolls == "X"
+    
+    def is_spare(self):
+        return "/" in self.rolls
+    
+
+no_strike_frame = Frame("34")
+
+
+
+strike_frame = Frame('X')
 
 
 
 
 
 def test_frame_not_strike():
-    assert frame.is_strike() == False
+    assert no_strike_frame.is_strike() == False
+
+def test_frame_is_strike():
+    assert strike_frame.is_strike() == True
+
+def test_frame_is_not_spare():
+    assert no_strike_frame.is_spare() == False
