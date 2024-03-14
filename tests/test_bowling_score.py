515c544c33af06a638b6295c0865bee2721205b2
Dom Patmore <dom@pilifter.com>
2024-03-14 11:29:45 +0000

    test score 34 => 7

--- tests/test_bowling_score.py
+++ tests/test_bowling_score.py
@@ -1,14 +1,16 @@
 def get_score_from_frame(frame):
     if frame == "X":
         return 10
-    else:
-        return 0
+    first_roll = int(frame[0])
+    second_roll = int(frame[1])
+    return first_roll + second_roll
 
-# test a single frame with a strike
 def test_strike_equals_10():
     assert get_score_from_frame("X") == 10
 
-# test a single frame without strike
+
 def test_simple_frame_no_score():
     assert get_score_from_frame("00") == 0
 
+def test_simple_frame_with_score():
+    assert get_score_from_frame("34") == 7
