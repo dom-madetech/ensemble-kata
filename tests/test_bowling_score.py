a1ef4134bf1f99c3676c3fdf414145d4dc7109b3
Dom Patmore <dom@pilifter.com>
2024-03-14 11:14:14 +0000

    refacgor test

--- tests/test_bowling_score.py
+++ tests/test_bowling_score.py
@@ -1,13 +1,14 @@
-def get_score_from_roll(roll):
-    if roll == "X":
+def get_score_from_frame(frame):
+    if frame == "X":
         return 10
     else:
         return 0
 
 # test a single frame with a strike
 def test_strike_equals_10():
-    assert get_score_from_roll("X") == 10
+    assert get_score_from_frame("X") == 10
 
 # test a single frame without strike
 def test_simple_frame_no_score():
-    assert get_score_from_roll("00") == 0
+    assert get_score_from_frame("00") == 0
+
